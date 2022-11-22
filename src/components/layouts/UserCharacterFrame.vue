7da7b82c7221b7687cefc2ce4bb57eed16719ef2
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-11-22 20:32:23 +0900

    Update MyPage with Layouts

---
+++ src/components/layouts/UserCharacterFrame.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="Frame cGG"></div>
+</template>
+
+<script>
+export default {
+  name: 'UserCharacterFrame',
+  components: {},
+  data() {
+    return {
+      sampleData: ''
+    }
+  },
+  setup() {},
+  created() {},
+  mounted() {},
+  unmounted() {},
+  methods: {}
+}
+</script>
+
+<style scoped>
+.Frame {
+  display: block;
+  width: 1280px;
+  height: 900px;
+  /* 인라인블록 사이 4px간격 쉬운 해소방법 필요 */
+  margin-top: -4px;
+}
+</style>
--- src/views/MyPageView.vue
+++ src/views/MyPageView.vue
@@ -1,9 +1,16 @@
 <template>
-  <div>MyPageView</div>
+  <div>
+    <user-post-frame />
+    <auth-frame />
+    <user-character-frame />
+  </div>
 </template>
 <script>
+import UserPostFrame from '@/components/layouts/UserPostFrame.vue'
+import AuthFrame from '../components/layouts/AuthFrame.vue'
+import UserCharacterFrame from '../components/layouts/UserCharacterFrame.vue'
 export default {
-  components: {},
+  components: { UserPostFrame, AuthFrame, UserCharacterFrame },
   data() {
     return {
       sampleData: ''
