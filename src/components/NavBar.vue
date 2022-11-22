12ba0d36e16f2354c882e973def38477ae7da5ed
Hajun Song <85068289+FoocoJun@users.noreply.github.com>
2022-11-22 20:38:41 +0900

    Add Tmp router-links at NavBar to move easy

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -3,6 +3,15 @@
     <div class="m40 tXL">
       <router-link to="/">대충 로고</router-link>
     </div>
+    <!-- 개발 중 이동 용이성을 위한 임시 라우트 링크 -->
+    <div class="m20 tS">
+      <router-link to="/user/asd">마이페이지</router-link> |
+      <router-link to="/create/work">작품만들기</router-link> |
+      <router-link to="/create/character">인물만들기</router-link> |
+      <router-link to="/xcvs/detail">상세페이지</router-link> |
+      <router-link to="/xcvs/write">작품글쓰기</router-link>
+    </div>
+    <!-- 개발 중 이동 용이성을 위한 임시 라우트 링크 -->
     <div class="m40 tM">
       <span class="m20">로그인</span> |
       <span class="m20">회원가입</span>
